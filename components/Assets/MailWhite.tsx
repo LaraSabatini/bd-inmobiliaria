a324f96f63b6fbd13967247517dbe15512a4d563
Lara <sabatinilara@gmail.com>
2022-05-18 18:58:14 -0300

    added icons

---
+++ components/Assets/MailWhite.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+function MailWhite() {
+  return (
+    <svg
+      width="30"
+      height="24"
+      viewBox="0 0 30 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M27 24H3C1.34315 24 0 22.6569 0 21V2.8695C0.0699125 1.26382 1.3928 -0.00151856 3 1.36787e-06H27C28.6569 1.36787e-06 30 1.34315 30 3V21C30 22.6569 28.6569 24 27 24ZM3 5.802V21H27V5.802L15 13.8L3 5.802ZM4.2 3L15 10.2L25.8 3H4.2Z"
+        fill="white"
+      />
+    </svg>
+  )
+}
+
+export default MailWhite
