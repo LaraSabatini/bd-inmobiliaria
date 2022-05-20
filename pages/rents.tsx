5224594a4b1f283a8268a2151295fdd9a2c7551d
Lara <sabatinilara@gmail.com>
2022-05-20 20:13:59 -0300

    added footer to pages

--- pages/rents.tsx
+++ pages/rents.tsx
@@ -1,12 +1,14 @@
 import React from "react"
 import Navbar from "../components/NavBar"
 import Title from "../components/Title"
+import Footer from "../components/Footer"
 
 const rents = () => {
   return (
     <>
       <Navbar />
       <Title text="Nuestras propiedades en alquiler" />
+      <Footer />
     </>
   )
 }
--- pages/sell.tsx
+++ pages/sell.tsx
@@ -1,12 +1,14 @@
 import React from "react"
 import Navbar from "../components/NavBar"
 import Title from "../components/Title"
+import Footer from "../components/Footer"
 
 const sell = () => {
   return (
     <>
       <Navbar />
       <Title text="Nuestras propiedades en venta" />
+      <Footer />
     </>
   )
 }
