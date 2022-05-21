c11d3af30aa44587145cf9f666c6f1b40a7cc83a
Lara <sabatinilara@gmail.com>
2022-05-20 21:30:37 -0300

    added comment for trial

--- pages/rents.tsx
+++ pages/rents.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import Navbar from "../components/NavBar"
 import Title from "../components/Title"
 import Footer from "../components/Footer"
+// import PropertyCard from "../components/PropertyCard"
 
 const rents = () => {
   return (
