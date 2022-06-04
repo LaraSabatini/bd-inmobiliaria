b701c6c367c58c03d22d7b9ec1a436a3bd417361
Lara <sabatinilara@gmail.com>
2022-06-04 14:50:12 -0300

    changed _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,39 @@
+import React from "react"
 import Document, { Html, Head, Main, NextScript } from "next/document"
+import { ServerStyleSheet } from "styled-components"
 
-class MyDocument extends Document {
+export default class CustomDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+
   render() {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <meta charSet="utf-8" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100&display=swap"
             rel="stylesheet"
@@ -20,5 +48,3 @@ class MyDocument extends Document {
     )
   }
 }
-
-export default MyDocument
